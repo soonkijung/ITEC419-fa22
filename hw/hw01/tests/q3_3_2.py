a79fe15674999e7fa90883d96b95f9f366b1aebb
soonkijung <soonkijung@gmail.com>
2022-08-27 06:21:06 +0000

    Connect Google Colab and Drive

---
+++ hw/hw01/tests/q3_3_2.py
@@ -0,0 +1,17 @@
+test = {   'name': 'q3_3_2',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> # Fill in the line that currently says;\n>>> #   predicted_distance_m = ...;\n>>> # in the cell above.;\n>>> predicted_distance_m != ...\nTrue',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> # Compute predicted_distance_m using the formula in the text;\n'
+                                               '>>> # above.  Hint: it should start with something like this:;\n'
+                                               '>>> #   predicted_distance_m = (1/2) * gravity_constant ...;\n'
+                                               '>>> round(predicted_distance_m, 5) == 1.17022\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> round(difference, 5)  == 0.04022\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
