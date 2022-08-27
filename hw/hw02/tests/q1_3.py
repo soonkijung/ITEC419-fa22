99598e34adcb2325ff5d571670b6912e76f12994
soonkijung <soonkijung@gmail.com>
2022-08-27 06:02:53 +0000

    Connect Google Colab and Drive

---
+++ hw/hw02/tests/q1_3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_3',
+    'points': 4,
+    'suites': [   {   'cases': [   {'code': ">>> with_commas == 'Eats, Shoots, and Leaves'\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> without_commas == 'Eats Shoots and Leaves'\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw02/tests/q2_5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_5', 'points': 4, 'suites': [{'cases': [{'code': '>>> min_of_birth_years == 1732\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
