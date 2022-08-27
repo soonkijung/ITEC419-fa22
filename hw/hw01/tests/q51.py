453e3e2a181497082080157b46287f7d36223a2d
Soon Ki Jung <soonki.jung@gmail.com>
2022-08-27 15:27:30 +0900

    Add files via upload

---
+++ hw/hw01/tests/q51.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q51',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> round(paola_distance_from_average_m, 5) == 0.072\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
