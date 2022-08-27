3080f15a4ba11c47261f0ee1c001108750c476a6
Soon Ki Jung <soonki.jung@gmail.com>
2022-08-27 15:26:00 +0900

    Delete hw/tests directory

--- hw/tests/q5_1_1.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q5_1_1',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> round(min_height_difference, 5) == 0.04\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
