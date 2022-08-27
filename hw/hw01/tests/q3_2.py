d998fe570c7b1af556dbd35e51e9eed40ac1db3f
soonkijung <soonkijung@gmail.com>
2022-08-27 16:47:46 +0000

    Connect Google Colab and Drive

--- hw/hw01/tests/q3_2.py
+++ hw/hw01/tests/q3_2.py
@@ -1,3 +1,3 @@
-test = {   'name': 'q51',
+test = {   'name': 'q3_2',
     'points': 1,
     'suites': [{'cases': [{'code': '>>> round(paola_distance_from_average_m, 5) == 0.072\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
