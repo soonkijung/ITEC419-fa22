48996ebf2807d267e610f177eacbfe74ad9767e6
soonkijung <soonkijung@gmail.com>
2022-08-30 09:25:31 +0000

    Connect Google Colab and Drive

---
+++ hw/hw03/tests/q2_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_1', 'points': 4, 'suites': [{'cases': [{'code': '>>> 0 < us_birth_rate < 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
