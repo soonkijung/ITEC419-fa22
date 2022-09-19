523e84182b29f80f39437dba38b1d6bccd92d193
soonkijung <soonkijung@gmail.com>
2022-09-19 01:16:22 +0000

    Connect Google Colab and Drive

---
+++ hw/hw02/tests/q1_1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_1',
+    'points': 4,
+    'suites': [   {   'cases': [{'code': '>>> np.allclose(weird_numbers, np.array([   -2,    12,     3, 15625]), rtol=1e-03, atol=1e-03) == True\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
