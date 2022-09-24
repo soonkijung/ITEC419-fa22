4e18994745bfd074b70efe677fdb514c95184740
soonkijung <soonkijung@gmail.com>
2022-09-24 22:21:44 +0000

    Connect Google Colab and Drive

---
+++ hw/hw03/tests/q3_3.py
@@ -0,0 +1,1 @@
+test = {'name': 'q3_3', 'points': 4, 'suites': [{'cases': [{'code': '>>> 1 <= unit_meaning <= 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
