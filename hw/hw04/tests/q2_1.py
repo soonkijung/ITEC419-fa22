5f0f47f2f2985a6c4a78f8d51f5301e0b52c51f9
soonkijung <soonkijung@gmail.com>
2022-10-04 08:36:46 +0000

    Connect Google Colab and Drive

---
+++ hw/hw04/tests/q2_1.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q2_1',
+    'points': 9,
+    'suites': [   {   'cases': [   {'code': '>>> job_titles.num_columns\n2', 'hidden': False, 'locked': False},
+                                   {'code': '>>> job_titles.num_rows\n6', 'hidden': False, 'locked': False},
+                                   {'code': '>>> # Make sure that you have the correct column labels!\n>>> np.asarray(job_titles.labels).item(1) != "Job full_array"\nFalse',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> # Make sure that you have the correct column labels!\n>>> np.asarray(job_titles.labels).item(1) == "Jobs"\nFalse', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
