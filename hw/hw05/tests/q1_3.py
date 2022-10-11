5dba86abdaccb1cf9f87188e290ec044c4e9699c
soonkijung <soonkijung@gmail.com>
2022-10-11 09:26:01 +0000

    Connect Google Colab and Drive

---
+++ hw/hw05/tests/q1_3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_3',
+    'points': 20,
+    'suites': [   {   'cases': [   {'code': ">>> 'did_cal_lose' in globals()\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> did_cal_lose(final_scores.row(1))\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
